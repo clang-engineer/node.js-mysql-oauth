1415cc4bc3a20448f972e0b6d3f6d845555d5bc9
orez <yorez@naver.com>
2018-09-24 16:38:35 +0900

    C'R'UD : mysql로 상세보기 구현

--- main.js
+++ main.js
@@ -4,7 +4,15 @@ var url = require('url');
 var qs = require('querystring');
 var template = require('./lib/template.js')
 var path = require('path');
-var sanitizeHtml=require('sanitize-html');
+var sanitizeHtml = require('sanitize-html');
+var mysql = require('mysql');
+var db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password:'123456',
+    database:'nodejs'
+});
+db.connect();
 
 var app = http.createServer(function (request, response) {
     var _url = request.url;
@@ -13,28 +21,27 @@ var app = http.createServer(function (request, response) {
 
     if (pathname === '/') {
         if (queryData.id === undefined) {
-            fs.readdir('./data', function (error, filelist) {
+            db.query('SELECT * FROM topic', function (error, topics) {
                 var title = 'WELCOME';
                 var description = 'make coding with node.js!!';
-                var list = template.List(filelist);
+                var list = template.List(topics);
                 var html = template.HTML(title, list, description,
                     `<a href="/create">CREATE</a>`);
                 response.writeHead(200);
                 response.end(html);
             });
         } else {
-            fs.readdir('./data', function (error, filelist) {
+            db.query('SELECT * FROM topic', function (error, topics) {
                 var filterID = path.parse(queryData.id).base;
-                fs.readFile(`data/${filterID}`, 'utf8', function (err, description) {
-                    var title = queryData.id;
-                    var sanitizeTitle=sanitizeHtml(title);
-                    var sanitizeDescription=sanitizeHtml(description,{allowedTags:['h1']});
-                    var list = template.List(filelist);
+                db.query('SELECT * FROM topic WHERE id=?',[filterID], function (error2, topic) {
+                    var sanitizeTitle = sanitizeHtml(topic[0].title);
+                    var sanitizeDescription = sanitizeHtml(topic[0].description, { allowedTags: ['h1'] });
+                    var list = template.List(topics);
                     var html = template.HTML(sanitizeTitle, list, sanitizeDescription,
                         `<a href="/create">CREATE</a>
-                        <a href="/update?id=${title}">UPDATE</a>
+                        <a href="/update?id=${queryData.id}">UPDATE</a>
                         <form action="/delete_process" method="post">
-                        <input type="hidden" name="id" value="${title}">
+                        <input type="hidden" name="id" value="${queryData.id}">
                         <input type="submit" value="delete">
                         </form>`);
                     response.writeHead(200);
@@ -116,7 +123,7 @@ var app = http.createServer(function (request, response) {
         request.on('end', function () {
             var post = qs.parse(body);
             var id = post.id;
-            var filterID=path.parse(id).base;
+            var filterID = path.parse(id).base;
             fs.unlink(`data/${filterID}`, function (error) {
                 response.writeHead(302, { Location: `/` });
                 response.end();
